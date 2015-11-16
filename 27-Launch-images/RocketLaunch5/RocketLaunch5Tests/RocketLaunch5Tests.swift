59b14c438c05ca58e0da86872ecee60a4802d8b9
wj2061 <wj2061@163.com>
2015-11-16 10:54:17 +0800

    add day-27 launch images

---
+++ 27-Launch-images/RocketLaunch5/RocketLaunch5Tests/RocketLaunch5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RocketLaunch5Tests.swift
+//  RocketLaunch5Tests
+//
+//  Created by WJ on 15/11/16.
+//  Copyright © 2015年 wj. All rights reserved.
+//
+
+import XCTest
+@testable import RocketLaunch5
+
+class RocketLaunch5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
